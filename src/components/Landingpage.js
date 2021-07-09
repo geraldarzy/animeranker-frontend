40b74d1ce25d31d04d44b50ca7bd49e13e5a9cb3
Gerald Arzola <geraldarzola@gmail.com>
2021-07-09 19:37:07 -0400

    landing page

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -4,7 +4,14 @@ function Landingpage(){
     return(
         <>
         <div id='landingpage' >
-            landingpage
+            <div>
+                Anime - Ranker
+            </div>
+            <br/>
+            <div>
+                What would you like to rate?
+            </div>
+            <button></button>
         </div>
         </>
     )
