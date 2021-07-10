89a7cad7d99ba11893e7bd2e91e2fdc7dc999cb9
glennanj1 <glennan20@gmail.com>
2021-07-10 16:59:07 -0400

    dynamic rank list

--- src/actions/fetchCards.js
+++ src/actions/fetchCards.js
@@ -1,10 +1,10 @@
 import {url} from '../index'
 
-export function fetchCards() {
+export function fetchCards(genre) {
     return (dispatch) => {
-        // dispatch({ type: 'LOADING_CARDS' });
+        dispatch({ type: 'LOADING_CARDS' });
 
-        fetch(`${url}/genres/Movie`)
+        fetch(`${url}/genres/${genre}`)
         .then(r => r.json())
         .then(res => dispatch({ type: 'ADD_CARDS', res }));
         
