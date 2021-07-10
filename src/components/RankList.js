9126a25e5208e5800dabcb0b076a0242af03fe00
glennanj1 <glennan20@gmail.com>
2021-07-10 16:59:55 -0400

    changed rank movies to rank list

---
+++ src/components/RankList.js
@@ -0,0 +1,44 @@
+import "./RankShows.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import ItemCard from "./ItemCard";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCards } from "../actions/fetchCards";
+import TierRows from "./TierRows";
+import { Spinner } from "react-bootstrap";
+
+function RankList(props) {
+  // const [list, setList] = useState([])
+  console.log(props);
+  const rankedList = useSelector((data) => data.cards.data);
+  const loading = useSelector((data) => data.loading);
+  console.log(loading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // fetch('https://anime-rankr.herokuapp.com/genres/Movie').then(resp=>resp.json()).then(data=>{
+    //     console.log(data.data.slice(0,20));
+    //     setList(data.data.slice(0,20));
+    // }) now we dispath action => to fetch data => to dispatch to store => then we useSelect to get that info here to us
+    dispatch(fetchCards(props.genre));
+  }, []);
+  return (
+    <>
+      <TierRows />
+      {rankedList !== undefined ? (
+        rankedList.map((item) => (
+          <ItemCard key={item.id} imgUrl={item.attributes.image_url} />
+        ))
+      ) : (
+        <Spinner animation="border" variant="primary" />
+      )}
+      {/* {loading ? 
+                 <h1>Loading...</h1> :
+                rankedList.map(item=>(
+                <ItemCard key={item.id}imgUrl={item.attributes.image_url}/>
+            )) 
+            } */}
+    </>
+  );
+}
+
+export default RankList;
