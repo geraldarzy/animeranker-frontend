5a088a9526e58a2b7f705c9e402798c05a35c655
glennanj1 <glennan20@gmail.com>
2021-07-10 17:00:16 -0400

    added loading reducer

--- src/reducers/cardReducer.js
+++ src/reducers/cardReducer.js
@@ -1,6 +1,13 @@
 const cardReducer = (state = { cards: [], loading: false}, action) => {
     switch(action.type) {
 
+        case 'LOADING_CARDS':
+            return {
+                ...state,
+                cards: state.cards,
+                loading: true
+            }
+
         case 'ADD_CARDS':
             return {
                 ...state,
