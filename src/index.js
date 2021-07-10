f78850b1227817cdefa836e84b701ace7ed67bd9
glennanj1 <glennan20@gmail.com>
2021-07-10 15:29:00 -0400

    wrap index in provider store

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router}  from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import {HTML5Backend} from 'react-dnd-html5-backend';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import cardReducer from './reducers/cardReducer'
+
+export const url = 'https://anime-rankr.herokuapp.com'
 
 ReactDOM.render(
   <DndProvider backend={HTML5Backend}>
     <React.StrictMode>
       <Router>
-        <App/>
+        <Provider store={createStore(cardReducer, applyMiddleware(thunk))}>
+          <App/>
+        </Provider>
       </Router>
     </React.StrictMode>
   </DndProvider>,
