6544ac3ab0a7dc841fe3f197e1dc611d2cde0225
glennanj1 <glennan20@gmail.com>
2021-07-10 16:59:18 -0400

    removed nav and added to app

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -8,25 +8,6 @@ function Landingpage(){
     const history = useHistory();
     return(
         <>
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">Anime-Ranker</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="#link">Rankings</Nav.Link>
-                    <NavDropdown title="Categories" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Top 20 Anime Movies</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Top 12 Anime Shows</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
         <div id='landingpage' >
         <img src={allAnimeLogo} alt='anime-ranker logo'style={{width:'100%', height:'20rem'}}/>
             <div className='main-container'>
