df92c17c4e4c54eb3cedea6123aef970b842a45b
Radiyah <rsabur58@gmail.com>
2021-07-09 20:35:42 -0400

    logo added

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,19 +1,17 @@
+import logo from '../cover.png'
 
 function Landingpage(){
 
-    return(
-        <>
+    return(        
         <div id='landingpage' >
-            <div>
-                Anime - Ranker
+            <div className='heading'>
+                <img src={logo} alt='anime-ranker logo'style={{width:'100%', height:'20rem'}}/>
             </div>
-            <br/>
-            <div>
+            <div className='main-container'>
                 What would you like to rate?
             </div>
             <button></button>
         </div>
-        </>
     )
 }
 
